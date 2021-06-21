991a3098f626e3d0ba22f40f3fd373464873e019
Rawnaqaburumman <engrawnaqaburummn@gmail.com>
2021-06-22 00:01:16 +0300

    editting js file

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 let ans = prompt('welcome all , What your favourite languge ?');
-alert('Oh graet')
+alert('Oh graet '+ ans + ' it\'s very intrsting language');
+let ans2 = prompt('What is your favourite book ?');
+alert (ans2 +' is a graet book');
+let ans3 = prompt('What is the esiset languge for learn to you?')
+alert ('yes,I deal with you '+ ans3+ 'is very nice');
+let ans4=prompt('what is your favourite auithor ?')
+alert( 'Great!')
